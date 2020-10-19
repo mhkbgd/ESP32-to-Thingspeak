ec506092bc68d7c67b576dc42882a045dac4111f
makhan <makhan@ttu.ee>
2020-10-20 00:04:14 +0300

    remove build files

--- build/expat/expat/expat/lib/loadlibrary.d
+++
@@ -1,3 +0,0 @@
-expat/expat/lib/loadlibrary.o: \
- C:/msys32/home/mhkbg/esp/esp-idf/components/expat/expat/expat/lib/loadlibrary.c
-/home/mhkbg/esp/esp-idf/components/expat/expat/expat/lib/loadlibrary.c:
--- build/expat/expat/expat/lib/xmltok_impl.d
+++
@@ -1,3 +0,0 @@
-expat/expat/lib/xmltok_impl.o: \
- C:/msys32/home/mhkbg/esp/esp-idf/components/expat/expat/expat/lib/xmltok_impl.c
-/home/mhkbg/esp/esp-idf/components/expat/expat/expat/lib/xmltok_impl.c:
